f12a566cd910a553bcbee17f61285bb8ad5c35fe
Jesse <jessebot.git@gmail.com>
2024-05-05 20:41:14 -0700

    feat: generate epub

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/JessebotX/mkbk
 go 1.22.1
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-shiori/go-epub v1.2.1 // indirect
+	github.com/gofrs/uuid/v5 v5.0.0 // indirect
+	github.com/vincent-petithory/dataurl v1.0.0 // indirect
 	github.com/yuin/goldmark v1.7.1 // indirect
 	github.com/yuin/goldmark-meta v1.1.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- renderer.go
+++ renderer.go
@@ -4,12 +4,16 @@ import (
 	"html/template"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
+
+	epub "github.com/go-shiori/go-epub"
 )
 
 const (
 	IndexTemplateName   = "index.html"
 	ChapterTemplateName = "_chapter.html"
+	RSSFeedTemplateName = "_rss.xml" // TODO implement RSS feeds
 )
 
 func RenderBookToHTMLSite(inputDir, outputDir string, book *Book) error {
@@ -32,35 +36,62 @@ func RenderBookToHTMLSite(inputDir, outputDir string, book *Book) error {
 		return err
 	}
 
-	indexFile, err := os.Create(filepath.Join(outputDir, IndexTemplateName))
-	if err != nil {
-		return err
-	}
-
+	// copy other layout files (theme-specific stylesheets, images, etc.)
 	err = copyDirectoryToOutput(layoutDir, outputDir, []string{IndexTemplateName, ChapterTemplateName, "README.md"})
 	if err != nil {
 		return err
 	}
 
-	// create index
-	err = indexTemplate.Execute(indexFile, book)
+	// create index file using index template
+	indexFile, err := os.Create(filepath.Join(outputDir, IndexTemplateName))
 	if err != nil {
 		return err
 	}
 	defer indexFile.Close()
 
-	// chapters
+	// write index index
+	err = indexTemplate.Execute(indexFile, book)
+	if err != nil {
+		return err
+	}
+
+	// begin writing book epub file
+	// TODO support images in epub
+	bookEpub, err := epub.NewEpub(book.Title)
+	if err != nil {
+		return err
+	}
+
+	// parse chapters
 	for _, chapter := range book.Chapters {
+		// create chapter html file
 		chapterFile, err := os.Create(filepath.Join(outputDir, chapter.Slug+".html"))
 		if err != nil {
 			return err
 		}
 		defer chapterFile.Close()
 
+		// write chapter html
 		err = chapterTemplate.Execute(chapterFile, &chapter)
 		if err != nil {
 			return err
 		}
+
+		// write to epub
+		sectionContent := "<h1>" + chapter.Title + "</h1>" + string(chapter.ContentHTML)
+		_, err = bookEpub.AddSection(sectionContent, chapter.Title, "", "")
+		if err != nil {
+			return err
+		}
+	}
+
+	sanitizer := regexp.MustCompile("([^a-zA-Z0-9]+)")
+	bookEpubName := sanitizer.ReplaceAllString(book.Title, "-")
+	bookEpubPath := filepath.Join(outputDir, strings.ToLower(bookEpubName+".epub"))
+
+	err = bookEpub.Write(bookEpubPath)
+	if err != nil {
+		return err
 	}
 
 	return nil
