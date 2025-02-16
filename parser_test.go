80216ac70c1120eafe09e97a14e715f319d7ff7d
Jesse <jessebot.git@gmail.com>
2025-02-15 23:58:38 -0800

    test: add extra test case for extra params in collection config

--- parser_test.go
+++ parser_test.go
@@ -27,3 +27,39 @@ func TestUnmarshalCollectionBasic(t *testing.T) {
 	}
 }
 
+func TestUnmarshalCollectionWithExtraParams(t *testing.T) {
+	collection := Collection{}
+	err := Unmarshal(
+		[]byte(`title: random title
+extratag: 2
+1: true`),
+		&collection)
+
+	if err != nil {
+		t.Fatalf(`Unmarshal(...) = %v, want nil, error`, err)
+	}
+
+	{
+		got := collection.Title
+		want := "random title"
+		if got != want {
+			t.Fatalf(`collection.Title = %v, want %v, error`, got, want)
+		}
+	}
+	{
+		got := collection.Params["extratag"].(int)
+		want := 2
+		if got != want {
+			t.Fatalf(`collection.Params["extratag"].(int) = %v, want %v, error`, got, want)
+		}
+	}
+
+	{
+		got := collection.Params["1"].(bool)
+		want := true
+		if got != true {
+			t.Fatalf(`collection.Params["1"].(bool) = %v, want %v, error`, got, want)
+		}
+	}
+}
+
